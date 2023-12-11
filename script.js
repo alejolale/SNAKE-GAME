e71c760c4665902f8e519b4ae4deb7bbb7e37d24
Sergio Prieto <sergio@yieldstudio.fr>
2023-12-11 22:24:58 +0100

    add snake direction

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 // define html elements
 const board = document.getElementById('game-board');
+const instructionText = document.getElementById('instruction-text');
+const logo = document.getElementById('logo');
 
 // Define game variables
 const gridSize =  20;
 let snake = [{x: 10, y: 10}];
 let food = generateFood();
 let direction =  'right';
-
+let gameInterval;
+let gameSpeedDelay = 200;
+let gameStarted =  false;
 
 //draw game map, snake, food
 function draw() {
@@ -56,30 +60,81 @@ function generateFood() {
 
 // moving the snake
 function move() {
-    const head = {...snake[0]};
-
+    const head = { ...snake[0] };
     switch (direction) {
-        case 'up':
-            head.y--;
-            break;
-        case 'down':
-            head.y++;
-            break;
-        case 'left':
-            head.x--;
-            break;
-        case 'right':
-            head.x++;
-            break;    
+      case 'up':
+        head.y--;
+        break;
+      case 'down':
+        head.y++;
+        break;
+      case 'left':
+        head.x--;
+        break;
+      case 'right':
+        head.x++;
+        break;
     }
-
+  
     snake.unshift(head);
-
-    snake.pop();
-}
+  
+    //   snake.pop();
+  
+    if (head.x === food.x && head.y === food.y) {
+      food = generateFood();
+      increaseSpeed();
+      clearInterval(gameInterval); // Clear past interval
+      gameInterval = setInterval(() => {
+        move();
+        checkCollision();
+        draw();
+      }, gameSpeedDelay);
+    } else {
+      snake.pop();
+    }
+  }
 
 // test moving
-setInterval(()=>{
-    move();//move first
-    draw(); //then draw new position
-}, 200)
+// setInterval(()=>{
+//     move();//move first
+//     draw(); //then draw new position
+// }, 200)
+
+// start game function
+function startGame() {
+    gameStarted = true; // Keep track of a running game
+    instructionText.style.display = 'none';
+    logo.style.display = 'none';
+    gameInterval = setInterval(() => {
+      move();
+     //checkCollision();
+      draw();
+    }, gameSpeedDelay);
+  }
+
+// Keypress event listener
+function handleKeyPress(event) {
+    if (
+      (!gameStarted && event.code === 'Space') ||
+      (!gameStarted && event.key === ' ')
+    ) {
+      startGame();
+    } else {
+      switch (event.key) {
+        case 'ArrowUp':
+          direction = 'up';
+          break;
+        case 'ArrowDown':
+          direction = 'down';
+          break;
+        case 'ArrowLeft':
+          direction = 'left';
+          break;
+        case 'ArrowRight':
+          direction = 'right';
+          break;
+      }
+    }
+}
+
+document.addEventListener('keydown', handleKeyPress)
