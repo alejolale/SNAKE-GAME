a6872207d9fc5a5d652edcdf52f3f57338c295a9
Sergio Prieto <sergio@yieldstudio.fr>
2023-12-11 22:05:31 +0100

    moves snake to right

--- script.js
+++ script.js
@@ -2,21 +2,26 @@
 const board = document.getElementById('game-board');
 
 // Define game variables
+const gridSize =  20;
 let snake = [{x: 10, y: 10}];
+let food = generateFood();
+let direction =  'right';
+
 
 //draw game map, snake, food
 function draw() {
     board.innerHTML =  '';
     drawSnake();
+    drawFood();
 }
 
 //draw snake
 function drawSnake() {
     snake.forEach((segment) => {
         const snakeElement = createGameElement('div', 'snake');
+        setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
     });
-    setPosition(snakeElement, segment);
-    board.appendChild(snakeElement);
 }
 
 // create a snake or food cube/div
@@ -33,4 +38,48 @@ function setPosition(element, position) {
 }
 
 // testing draw function
-draw();
+//draw();
+
+// draw food function
+function drawFood() {
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+// generate food
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return {x, y};
+}
+
+// moving the snake
+function move() {
+    const head = {...snake[0]};
+
+    switch (direction) {
+        case 'up':
+            head.y--;
+            break;
+        case 'down':
+            head.y++;
+            break;
+        case 'left':
+            head.x--;
+            break;
+        case 'right':
+            head.x++;
+            break;    
+    }
+
+    snake.unshift(head);
+
+    snake.pop();
+}
+
+// test moving
+setInterval(()=>{
+    move();//move first
+    draw(); //then draw new position
+}, 200)
